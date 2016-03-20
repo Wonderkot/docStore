80f5aeda6a13133445ef19ffe72cc5406bd11019
ybogdanov <dante4645@yandex.ru>
2016-03-20 18:38:55 +0400

    рефакторинг

--- DocStore/Controllers/UserController.cs
+++ DocStore/Controllers/UserController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Text;
 using System.Web.Mvc;
 using DocStore.Models;
-using DocStore.Models.Nhibernate;
-using NHibernate.Criterion;
+using DocStore.Models.Manage;
 
 namespace DocStore.Controllers
 {
@@ -31,69 +29,16 @@ namespace DocStore.Controllers
             }
             Log.Debug($"Пользователь {user.Name} пытается войти в приложение.");
             if (!ModelState.IsValid) return View();
-            var session = NHibertnateSession.OpenSession();
-            if (session == null)
+            string errMsg = string.Empty;
+            if (UserManager.CheckUser(user, out errMsg))
             {
-                var errorMessage = "Ошибка подключения к базе данных";
-                Log.Error(errorMessage);
-                ModelState.AddModelError("error", errorMessage);
-                return View();
-            }
-            User userIndDb;
-            try
-            {
-                var userCrit = session.CreateCriteria<User>();
-                Log.Info($"Выполняется поиск пользователя {user.Name} в базе.");
-                userCrit.Add(Restrictions.Eq("Name", user.Name));
-                userIndDb = userCrit.UniqueResult<User>();
-                if (userIndDb == null)
-                {
-                    throw new Exception($"Пользователь с именем {user.Name} не найден в базе.");
-                }
-                Log.Info($"Пользователь {user.Name} найден в базе, необходимо выполнить проверку пароля.");
-            }
-            catch (Exception e)
-            {
-                ModelState.AddModelError("error", e.Message);
-                Log.Error(e);
-                return View();
-            }
-            finally
-            {
-                Log.Debug("Выполняется закрытие sql-сессии");
-                session.Close();
-                Log.Debug("Закрытие sql-сессии заверншено");
-            }
-
-            Log.Debug("Производится вычисление хэша введенного пароля");
-            var computedHash = ComputeHash(user);
-            Log.Debug($"Полученное значение хэша:{computedHash}");
-
-            if (computedHash.Equals(userIndDb.Password))
-            {
-                Log.Info($"Пользователь {userIndDb.Name} может работать с документами. Роль пользователя: {userIndDb.Role.Name}");
                 return View("Documents");
             }
-            var message = "Неверный пароль.";
-            Log.Error(message);
-            ModelState.AddModelError("error", message);
+            //если возникли ошибки, то пишем о них
+            Log.Error($"При попытке авторизации возникла ошибка: {errMsg}");
+            ModelState.AddModelError("error", errMsg);
             return View();
         }
 
-        /// <summary>
-        /// Вычисление хша пароля, который ввел пользователь
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
-        private string ComputeHash(User user)
-        {
-            byte[] hash;
-            using (var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider())
-                hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(user.Password));
-            var sb = new StringBuilder();
-            foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
-            string computedHash = sb.ToString();
-            return computedHash;
-        }
     }
 }
---
+++ DocStore/Models/Manage/UserManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+using DocStore.Models.Nhibernate;
+using NHibernate.Criterion;
+
+namespace DocStore.Models.Manage
+{
+    /// <summary>
+    /// Класс для управления пользователями
+    /// </summary>
+    public class UserManager
+    {
+        private static readonly NLog.Logger Log = NLog.LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Проверка существования пользователя
+        /// </summary>
+        /// <param name="user">Проверяемый пользователь</param>
+        /// <seealso cref="User"/>
+        /// <param name="msg">Сообщение об ошибке</param>
+        /// <returns></returns>
+        public static bool CheckUser(User user, out string msg)
+        {
+            msg = string.Empty;
+            var session = NHibertnateSession.OpenSession();
+            if (session == null)
+            {
+                msg = "Ошибка подключения к базе данных";
+                return false;
+            }
+            User userIndDb;
+            try
+            {
+                var userCrit = session.CreateCriteria<User>();
+                Log.Info($"Выполняется поиск пользователя {user.Name} в базе.");
+                userCrit.Add(Restrictions.Eq("Name", user.Name));
+                userIndDb = userCrit.UniqueResult<User>();
+                if (userIndDb == null)
+                {
+                    throw new Exception($"Пользователь с именем {user.Name} не найден в базе.");
+                }
+                Log.Info($"Пользователь {user.Name} найден в базе, необходимо выполнить проверку пароля.");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+                msg = e.Message;
+                return false;
+            }
+            finally
+            {
+                Log.Debug("Выполняется закрытие sql-сессии");
+                session.Close();
+                Log.Debug("Закрытие sql-сессии завершено");
+            }
+            Log.Debug("Производится вычисление хэша введенного пароля");
+            var computedHash = ComputeHash(user);
+            Log.Debug($"Полученное значение хэша:{computedHash}");
+
+            if (computedHash.Equals(userIndDb.Password))
+            {
+                if (userIndDb.Role != null)
+                    Log.Info($"Пользователь {userIndDb.Name} может работать с документами. Роль пользователя: {userIndDb.Role.Name}");
+                return true;
+            }
+            msg = "Неверный пароль.";
+            return false;
+        }
+
+        /// <summary>
+        /// Вычисление хша пароля, который ввел пользователь
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        private static string ComputeHash(User user)
+        {
+            byte[] hash;
+            using (var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider())
+                hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(user.Password));
+            var sb = new StringBuilder();
+            foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
+            string computedHash = sb.ToString();
+            return computedHash;
+        }
+    }
+}
